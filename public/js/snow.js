bc3d3bda2345cf97a436d481b2630da45faa1bab
Brais Moure <mouredev@gmail.com>
2023-11-28 22:55:11 +0100

    Se modifica la velocidad de la nieve

--- public/js/snow.js
+++ public/js/snow.js
@@ -42,7 +42,7 @@ var snowStorm = (function (window, document) {
     this.flakeWidth = 15;           // Max pixel width reserved for snow element
     this.flakeHeight = 15;          // Max pixel height reserved for snow element
     this.vMaxX = 3;                 // Maximum X velocity range for snow
-    this.vMaxY = 5;                 // Maximum Y velocity range for snow
+    this.vMaxY = 4;                 // Maximum Y velocity range for snow
     this.zIndex = 0;                // CSS stacking order applied to each snowflake
 
     // --- "No user-serviceable parts inside" past this point, yadda yadda ---
