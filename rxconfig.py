0a7b1fdde31ab2a1b6282b7cfee01c0f56c73852
MoureDev <braismoure@mouredev.com>
2025-11-26 13:12:27 +0100

    aDEViento 2025

--- rxconfig.py
+++ rxconfig.py
@@ -3,6 +3,8 @@ import reflex as rx
 config = rx.Config(
     app_name="adeviento_web",
     deploy_url=None,
+    backend_port=None,
+    api_url=None,
     plugins=[rx.plugins.SitemapPlugin()],
     show_built_with_reflex=False,
 )
